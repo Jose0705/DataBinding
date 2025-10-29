b61d50a69969091781b151fe48f25c1d9dc6ee53
José Flores <24470240@campeche.tecnm.mx>
2025-10-29 11:18:11 -0600

    Agrega ejemplos de enlaze de datos a un objecto

---
+++ DataBinding.DataObject/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using DataBinding.DataObject.NewFolder;
+
+namespace DataBinding.DataObject
+{
+    public partial class MainPage : ContentPage
+    {
+        // conteo lleva el conteo de la aplic git statusacion
+        private Contador contador;
+        public MainPage()
+        {
+            InitializeComponent();
+            contador = new Contador();
+            BindingContext = contador;
+         
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            contador.Contar();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            contador.Reiniciar();
+        
+        }
+    }
+}
---
+++ DataBinding.DataObject/Models/Contador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DataBinding.DataObject.NewFolder
+{
+    public class Contador : INotifyPropertyChanged
+    {
+        private int _conteo;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public int Conteo
+        {
+            get => _conteo;
+
+            set
+            {
+
+                if (_conteo != value)
+                {
+
+                    _conteo = value;
+                    OnPropertyChanged(nameof(Conteo));
+
+                }
+            }
+        }
+        public Contador()
+
+        {
+
+            Conteo = 0;
+
+        }
+        public void Contar()
+        {
+            Conteo++;
+
+        }
+        public void Reiniciar()
+        {
+            Conteo = 0;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+               if (PropertyChanged != null)
+             {
+               PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            PropertyChanged? .Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        
+    }
+}
